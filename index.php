779a5d1f6bd83ddde2973043e16e1eccc97ea969
alicjanitecka <nitecka.alicja03@gmail.com>
2024-12-28 15:56:26 +0100

    Add PHP routing

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo 'Hi there students👋';
+
+require 'Routing.php';
+
+
+$path=trim($_SERVER['REQUEST_URI'], '/');
+$path=parse_url($path, PHP_URL_PATH);
+
+Routing::get('index', 'DefaultController');
+Routing::get('projects', 'DefaultController');
+Routing::run($path);
